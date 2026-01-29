bfcd435b9e352f290cbaf8dee0ddaa08ced454ea
Saba <64770978+github-saba@users.noreply.github.com>
2026-01-29 17:18:43 +0000

    Added a basic Swing application with JFrame (Java)

---
+++ Swing-Demo/SwingDemo1.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class SwingDemo1 {
+    public static void main(String[] args) {
+        // Always start Swing apps on the Event Dispatch Thread (EDT)
+
+         SwingUtilities.invokeLater(() -> {
+        	// This code runs on the Event Dispatch Thread
+    	    JFrame frame = new JFrame("My App");
+   	        frame.setSize(400, 300);
+   	        frame.setVisible(true);
+         });
+   }
+}

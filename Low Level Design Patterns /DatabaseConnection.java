7e7cc5f7224ba3d925f646a248818619196b9dd9
Saba <64770978+github-saba@users.noreply.github.com>
2026-04-13 19:40:41 +0100

    Add DatabaseConnection class with Singleton pattern
    Implemented Singleton pattern for DatabaseConnection to ensure a single instance.

---
+++ Low Level Design Patterns /DatabaseConnection.java
@@ -0,0 +1,17 @@
+/* Singleton pattern - It ensures a class has just one instance and provides a global access point to it.
+   Perfect for things like loggers or database connections where having multiple copies would cause chaos.
+   The Vibe: "There can be only one." */
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+
+    // Private constructor prevents others from using 'new'
+    private DatabaseConnection() {}
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+}

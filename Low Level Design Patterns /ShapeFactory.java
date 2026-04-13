3028283c250bf4f9e537c5c8d7726e177be0ca20
Saba <64770978+github-saba@users.noreply.github.com>
2026-04-13 20:09:35 +0100

    Refactor ShapeFactory to static method and improve comments
    Updated ShapeFactory to use static method and enhanced comments.

--- Low Level Design Patterns /ShapeFactory.java
+++ Low Level Design Patterns /ShapeFactory.java
@@ -1,10 +1,36 @@
-/* Factory Method Pattern - The Vibe: "I’ll decide what to give you based on what you ask for."
-Instead of the client calling new on a specific class, they call a "factory" that decides which object to instantiate.*/ 
+/* Factory Method Pattern (creational) - The Vibe: "I’ll decide what to give you based on what you ask for."
+Instead of the client calling new on a specific class, they call a "factory" that decides which object to instantiate.
+Defines an interface for creating objects but lets subclasses decide which class to instantiate, promoting loose coupling. */ 
 
 public class ShapeFactory {
-    public Shape getShape(String type) {
+    public static Shape getShape(String type) {
         if (type.equals("CIRCLE")) return new Circle();
         if (type.equals("SQUARE")) return new Square();
         return null;
+    }
+
+    public static void main(String args[]) {
+        Shape myShape = ShapeFactory.getShape("SQUARE");
+        myShape.draw(); /* The code doesn't care it's a circle, it just knows it's a "Shape" */
+    }
+}
+
+public interface Shape {
+    void draw(); // Every shape must implement this method
+}
+
+// Implementation 1
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle!");
+    }
+}
+
+// Implementation 2
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Square!");
     }
 }
